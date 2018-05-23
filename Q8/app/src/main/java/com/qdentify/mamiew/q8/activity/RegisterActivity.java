d811d5b08ea82b16f13a9ff6e6ddfe6fc2e91981
Lalita076 <lalita.076@mail.kmutt.ac.th>
2018-05-24 00:09:02 +0700

    Merge branch 'master' of https://github.com/Lalita076/Qdentify
    # Conflicts:
    #	Q8/.idea/caches/build_file_checksums.ser
    #	Q8/app/src/main/AndroidManifest.xml
    #	Q8/app/src/main/java/com/qdentify/mamiew/q8/fragment/TabThreeAccount.java

--- Q8/app/src/main/java/com/qdentify/mamiew/q8/activity/RegisterActivity.java
+++ Q8/app/src/main/java/com/qdentify/mamiew/q8/activity/RegisterActivity.java
@@ -16,6 +16,7 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -39,7 +40,7 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
         progressDialog = new ProgressDialog(this);
 
         firebaseAuth = FirebaseAuth.getInstance();
-        if(firebaseAuth.getCurrentUser() !=null){
+        if (firebaseAuth.getCurrentUser() != null) {
             finish();
             startActivity(new Intent(getApplicationContext(), MainActivity.class));
         }
@@ -52,42 +53,41 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
     private void initInstances() {
 
 
-        btnRegister = (Button)findViewById(R.id.btn_register);
-        btnToSignIn = (Button)findViewById(R.id.btn_signin);
+        btnRegister = (Button) findViewById(R.id.btn_register);
+        btnToSignIn = (Button) findViewById(R.id.btn_signin);
 
-        etEmail = (EditText)findViewById(R.id.et_mail);
-        etPassword = (EditText)findViewById(R.id.et_password);
+        etEmail = (EditText) findViewById(R.id.et_mail);
+        etPassword = (EditText) findViewById(R.id.et_password);
 
 
     }
 
     @Override
     public void onClick(View view) {
-        if (view==btnRegister){
-            register();
+        switch (view.getId()) {
+            case R.id.btn_register:
+                register();
+                break;
+            case R.id.btn_signin:
+                finish();
+                Intent singin = new Intent(getApplicationContext(), LoginActivity.class);
+                Toast.makeText(RegisterActivity.this, "go to Login", Toast.LENGTH_SHORT).show();
+                startActivity(singin);
+                break;
         }
-        if(view==btnToSignIn){
-            //login();
-            Intent singin = new Intent(getApplicationContext(),LoginActivity.class);
-            Toast.makeText(RegisterActivity.this, "go to Login",Toast.LENGTH_SHORT).show();
-            startActivity(singin);
-        }
-
-        Intent login = new Intent(RegisterActivity.this,MainActivity.class);
-        startActivity(login);
     }
 
     private void register() {
         String email = etEmail.getText().toString().trim();
         String password = etPassword.getText().toString().trim();
 
-        if(TextUtils.isEmpty(email)){
+        if (TextUtils.isEmpty(email)) {
             //emial is empty
-            Toast.makeText(this, "Please enter mail", Toast.LENGTH_SHORT).show();;
+            Toast.makeText(this, "Please enter mail", Toast.LENGTH_SHORT).show();
             return;
         }
-        if(TextUtils.isEmpty(password)){
-            Toast.makeText(this,"Please enter password",Toast.LENGTH_SHORT).show();
+        if (TextUtils.isEmpty(password)) {
+            Toast.makeText(this, "Please enter password", Toast.LENGTH_SHORT).show();
             return;
         }
         progressDialog.setMessage("Registering please wait..");
@@ -97,13 +97,16 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
-                        if(task.isSuccessful()){
+                        if (task.isSuccessful()) {
                             finish();
                             startActivity(new Intent(getApplicationContext(), MainActivity.class));
                             Toast.makeText(RegisterActivity.this, "Register Successfully", Toast.LENGTH_SHORT).show();
-                        }
-                        else{
-                            Toast.makeText(RegisterActivity.this, "Could not register.. please try again", Toast.LENGTH_SHORT).show();
+                        } else {
+                            if(task.getException() instanceof FirebaseAuthUserCollisionException){
+                                Toast.makeText(getApplicationContext(),"You are already register",Toast.LENGTH_SHORT);
+                            }else {
+                                Toast.makeText(getApplicationContext(), task.getException().toString(), Toast.LENGTH_SHORT).show();
+                            }
                         }
 
                     }
